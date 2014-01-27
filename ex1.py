77b92bd7bf5542caf36fbd267256895cfbfa3e5c
cdcd <caodan01@sohu.com>
2014-01-27 10:00:02 +0800

    11
    11

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print 'Hello World!'
+print 'Hello Again'
+print 'I like typing this'
+print 'this is fun'
+print "I'd much rather you 'not'"
+print 'I"said" do not touch this.'
+
+print '''aaa
+bbbb
+cccc 
+'''
+print "1234#123"
